f5398a416f03fc7d66312602707d885f0f4f6a04
ommiPutera <77653145+ommiPutera@users.noreply.github.com>
2021-03-13 21:39:16 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LoginAction } from "./redux/actions";
+import { connect } from "react-redux";
+import { API_URL } from "./helper";
+import ProductDetail from "./pages/User_Products_Detail";
+import Transaction from './pages/User_Transaction';
+import SignUp from "./pages/User_SignUp";
+import Home from './pages/User_Home';
+import Cart from './pages/User_Cart';
+import ManageProduct from './pages/Admin_ManageProduct';
+import NotFound from './pages/ErrorNotFound404';
+import axios from "axios";
+import './App.css';
+
+class App extends Component {
+  state = {
+    isLoading: true,
+  };
+  componentDidMount() {
+    let id = localStorage.getItem("id");
+    axios
+      .get(`${API_URL}/users/${id}`)
+      .then((res) => {
+        this.props.LoginAction(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/user_signup' exact component={SignUp} />
+          <Route path='/admin_manage_product' exact component={ManageProduct} />
+          <Route path="/cart" exact component={Cart} />
+          <Route path="/transaction" exact component={Transaction} />
+          <Route path="/product/:idprod" exact component={ProductDetail} />
+          <Route path='*' component={NotFound} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+const MaptstatetoProps = (state) => {
+  return {
+    dataUser: state.Auth,
+  };
+};
+export default connect(MaptstatetoProps, { LoginAction })(App);
